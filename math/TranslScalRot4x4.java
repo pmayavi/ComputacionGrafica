19ba0ea389f46f02c35ec9809eadf4296ab43704
pmayavi <pmayav@eafit.edu.co>
2023-09-16 14:45:12 -0500

    Camera fix

--- math/TranslScalRot4x4.java
+++ math/TranslScalRot4x4.java
@@ -12,6 +12,11 @@ public class TranslScalRot4x4 extends Matrix4x4 {
             double u, double v, double n,
             double cameraTx, double cameraTy) {
         super();
+        // Camara
+        matrix = Matrix4x4.times(this, new Translation4x4(u, v, n)).matrix;
+        matrix = Matrix4x4.times(this, new RotationX4x4(cameraTx)).matrix;
+        matrix = Matrix4x4.times(this, new RotationY4x4(cameraTy)).matrix;
+
         // Regresar al centro
         matrix = Matrix4x4.times(this, new Translation4x4(centerX + dx, centerY + dy, centerZ + dz)).matrix;
         // Scaling
@@ -22,11 +27,6 @@ public class TranslScalRot4x4 extends Matrix4x4 {
         matrix = Matrix4x4.times(this, new RotationZ4x4(thetaZ)).matrix;
         // Traslacion
         matrix = Matrix4x4.times(this, new Translation4x4(-centerX, -centerY, -centerZ)).matrix;
-
-        // Camara
-        matrix = Matrix4x4.times(this, new Translation4x4(u, v, n)).matrix;
-        matrix = Matrix4x4.times(this, new RotationX4x4(cameraTx)).matrix;
-        matrix = Matrix4x4.times(this, new RotationY4x4(cameraTy)).matrix;
     }
 
 }
