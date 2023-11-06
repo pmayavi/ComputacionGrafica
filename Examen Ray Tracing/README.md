350d0827d6aa538154db233870fcedef64c92f6d
pmayavi <pmayav@eafit.edu.co>
2023-11-06 15:20:51 -0500

    Gifs Examen

---
+++ Examen Ray Tracing/README.md
@@ -0,0 +1,6 @@
+# Examen final practico
+## Ray Tracing
+Por Pablo Maya Villegas y Andres Echeverri Jaramillo
+
+![Replit](https://raw.githubusercontent.com/pmayavi/ComputacionGrafica/main/Examen%20Ray%20Tracing/RayTracing_Replit.gif)  
+![Artefacto](https://raw.githubusercontent.com/pmayavi/ComputacionGrafica/main/Examen%20Ray%20Tracing/RayTracing_Artefacto.gif)  
--- Examen Ray Tracing/src/geometry/Plane.java
+++ Examen Ray Tracing/src/geometry/Plane.java
@@ -1,4 +1,5 @@
 package geometry;
+
 import math.Vector4;
 import display.Scene;
 import math.UVN4x4;
@@ -29,33 +30,33 @@ public class Plane implements IntersectableObject {
         this.colorIndex = colorIndex;
         this.materialIndex = materialIndex;
     }
-    
+
     public Solution intersect(Ray ray) {
         // Calcula el numerador de la ecuación de la intersección
-        double numerator = -(Vector4.dotProduct(ray.origin, n) + d);
+        double numerador = -(Vector4.dotProduct(ray.origin, n) + d);
         // Calcula el denominador de la ecuación de la intersección
-        double denominator = Vector4.dotProduct(ray.direction, n);
-    
+        double denominador = Vector4.dotProduct(ray.direction, n);
+
         // Verifica si el rayo es paralelo al plano (sin intersección)
-        if (denominator == 0) {
+        if (denominador == 0) {
             return null;
         }
-    
+
         // Calcula la distancia a lo largo del rayo al punto de intersección
-        double t = numerator / denominator;
-    
+        double t = numerador / denominador;
+
         // Verifica si la intersección ocurre detrás del origen del rayo
-        if (t < 0) {
+        if (t < 0)
             return null;
-        }
-    
+
         // Calcula las coordenadas del punto de intersección
-        Vector4 intersectionPoint = Vector4.add(ray.origin, Vector4.multiply(ray.direction, t));
-    
-        // Crea una instancia de Solution para almacenar la información de la intersección
-        Solution solution = new Solution(intersectionPoint, n, Scene.colors.get(colorIndex), Scene.materials.get(materialIndex));
-    
-        return solution;
+        Vector4 puntoInterseccion = Vector4.add(ray.origin, Vector4.multiply(ray.direction, t));
+
+        // Crea una instancia de Solution para almacenar la info de la intersección
+        Solution sol = new Solution(puntoInterseccion, n, Scene.colors.get(colorIndex),
+                Scene.materials.get(materialIndex));
+
+        return sol;
     }
 
     public void setCamera() {
@@ -71,16 +72,16 @@ public class Plane implements IntersectableObject {
         Vector4 p1p3 = Vector4.subtract(transformedP3, transformedP1);
         this.n = Vector4.crossProduct(p1p2, p1p3);
         this.n.normalize();
-        this.d = - Vector4.dotProduct(transformedP1, n);
+        this.d = -Vector4.dotProduct(transformedP1, n);
     }
 
     public String toString() {
-        String s = "a: " + this.n.vector[0] + " b: " + this.n.vector[1] + 
-            " c: " + this.n.vector[2] + " d: " + this.d;
+        String s = "a: " + this.n.vector[0] + " b: " + this.n.vector[1] +
+                " c: " + this.n.vector[2] + " d: " + this.d;
         return s;
     }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Vector4 p1 = new Vector4(-100, -100, -1000);
         Vector4 p2 = new Vector4(0, -100, -900);
         Vector4 p3 = new Vector4(100, -100, -1000);
